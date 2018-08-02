96b4b7b9afa0f94af9d8cc1eec19947300c192f8
Shachar Oz <shachar.oz@gmail.com>
2018-08-03 00:07:22 +0300

    menu system project

---
+++ Assets/Multiple Scene Based Project/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour {
+
+    private static SceneLoader instance;
+    internal static SceneLoader Instance { get { return instance; } }
+    
+    void Awake()
+    {
+        instance = this;
+    }
+
+
+
+
+    public void LoadScene(string scene_name)
+    {
+        Debug.Log("SceneLoader will now load scene named " + scene_name);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene_name);
+    }
+
+
+
+	
+	
+}
